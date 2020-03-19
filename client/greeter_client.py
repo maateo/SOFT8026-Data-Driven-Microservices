a9efef8e2c41f07c1376ad652ae813fddbb95446
Mateusz <15845263+maateo@users.noreply.github.com>
2020-03-19 16:38:53 +0000

    analysis works

--- client/greeter_client.py
+++ client/greeter_client.py
@@ -18,6 +18,7 @@ from __future__ import print_function
 import datetime
 import logging
 import random
+import re
 import time
 
 import grpc
@@ -38,8 +39,23 @@ def run():
 
         try:
             conn = redis.StrictRedis(host='redis', port=6379)
+            tweet_data = {
+                "target": str(response.target),
+                "id": str(response.id),
+                "date": str(response.date),
+                "flag": str(response.flag),
+                "user": str(response.user),
+                "text": str(response.text),
+                "word_length": str(len(re.findall(r'\w+', response.text)))
+            }
 
-            conn.set("tweets." + str(datetime.datetime.now()), response.text)
+            conn.hmset("tweets." + str(datetime.datetime.now()), tweet_data)
+            conn.set("3md." + str(datetime.datetime.now()), response.target, ex=180)
+
+            analise_totals(conn, response.text)
+            analise_most_of(conn, response.text)
+            analise_last_3_minutes(conn)
+
 
         except Exception as ex:
             print('Error:', ex)
@@ -54,6 +70,40 @@ def run():
         print()
         print()
         time.sleep(random.randint(1, 3))
+
+
+def analise_totals(conn, text):
+    word_count = len(re.findall(r'\w+', text))
+
+    vowels = 'aeiou'
+    vowel_count = 0
+
+    for letter in text.lower():
+        if letter in vowels:
+            vowel_count += 1
+
+    conn.incrby("total_vowel_count", vowel_count)
+    conn.incrby("total_word_count", word_count)
+
+
+def analise_last_3_minutes(conn):
+    keys = conn.scan_iter("3md*")
+    sentiments = [0, 0, 0, 0, 0]
+
+    for key in keys:
+        target = int(conn.get(key))
+        sentiments[target] += 1
+
+    conn.set("3_minute_sentiment", str(sentiments))
+
+def analise_most_of(conn, text):
+    word_count = len(re.findall(r'\w+', text))
+
+    ath_word_count = conn.get("all_time_high_word_count")
+
+    if ath_word_count is None or word_count > int(ath_word_count):
+        conn.set("all_time_high_word_count", word_count)
+        conn.set("all_time_high_word", text)
 
 
 if __name__ == '__main__':
--- web/output_logs.py
+++ web/output_logs.py
@@ -6,18 +6,125 @@ app = Flask(__name__)
 
 @app.route('/')
 def print_logs():
-    output = ''
+    output = '''
+        
+    '''
     try:
-        conn = redis.StrictRedis(host='redis', port=6379)
-        keys = conn.scan_iter("tweets*")
-        keys = sorted(keys, reverse=True)
+        conn = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
+        # keys = conn.scan_iter("tweets*")
+        # keys = sorted(keys, reverse=True)
 
-        for key in keys:
-            value = str(conn.get(key))
-            output += str(key) + " AAAA " + value + '<br>'  # Style the output lines
+        output += str(conn.get("3_minute_sentiment"))
+
+        output += "<br><br>"
+
+        output += str(conn.get("total_vowel_count"))
+        output += "<br>"
+        output += str(conn.get("total_word_count"))
+
+        output += "<br><br>"
+        output += output_tweets(conn)
+
+        output +=str(conn.get("all_time_high_word_count"))
+        output +=str(conn.get("all_time_high_word"))
+
+        # datakeys = conn.scan_iter("data*")
+        #
+        # for key in datakeys:
+        #     data = conn.hgetall(key)
+        #
+        # for key in keys:
+        #     value = str(conn.get(key))
+        #     # output += str(key) + " AAAA " + value + '<br>'  # Style the output lines
+        #     output += str(data) + " AAAA " + str(data) + '<br>'  # Style the output lines
     except Exception as ex:
         output = 'Error:' + str(ex)
     return output
+
+
+def output_tweets(conn):
+    keys = conn.scan_iter("tweets*")
+    keys = sorted(keys, reverse=True)
+
+    output = ''
+
+    for key in keys:
+        data = conn.hgetall(key)
+        # decoded_data = data.
+        output += str(data)
+
+    return output
+
+
+def combine_outputs():
+    output = """
+                <style> 
+              #wrapper {
+                display: flex;
+                border: 1px solid black;
+              }
+              #first {
+                border: 1px solid red;
+                width: 30%;
+                padding: 1em;
+              }
+              #second {
+                border: 1px solid green;
+                width: 70%;
+                padding: 1em;
+              }
+            </style>
+            
+            <div id="wrapper">
+            
+              <div id="first">
+            
+                <center><h3> Total Letters </h3></center>
+                Total Letters: 3
+            
+            
+                <center><h3> 3 Minute Sentiment </h3></center>
+                All good 
+            
+                <center><h3> Most of... </h3></center>
+                Tweet with most words: 
+                <br>
+                Word count: 5
+            
+              </div>
+            
+              <div id="second">
+            
+                <table style="width:100%">
+                  <tr>
+                    <td>
+                      <div>
+                        <center> 
+                          <small><i>joe1232</i></small>
+            
+                          <br>
+            
+                          I love this world
+            
+                          <br>
+            
+                          <small>
+                            <strong> Tweet Id: </strong> 75354
+                            <strong> Time analised: </strong> 11:30am
+                            <strong> Words:</strong> 20
+                          </small>
+                        </center>
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+
+
+
+
+    """
 
 
 if __name__ == '__main__':
