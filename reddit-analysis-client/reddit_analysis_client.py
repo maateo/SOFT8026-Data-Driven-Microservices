ae575d19ee00638e7d7f2cfc4d73a33c9841f72e
Mateusz <15845263+maateo@users.noreply.github.com>
2020-04-09 12:51:50 +0100

    Reddit shown on website

--- reddit-analysis-client/reddit_analysis_client.py
+++ reddit-analysis-client/reddit_analysis_client.py
@@ -28,30 +28,31 @@ def run():
                         "original_date": str(reddit_post.original_date),
                         "full_link": str(reddit_post.full_link),
                         "over_18": str(reddit_post.over_18),
-                        # "word_count": str(len(re.findall(r'\w+', reddit_post.text))),
+                        "word_count": str(len(re.findall(r'\w+', reddit_post.title))),
                         "time_analysed": str(datetime.datetime.now())
                     }
 
-                    # conn.hmset("reddit_posts." + str(datetime.datetime.now()), reddit_post_data)
-                    # conn.set("reddit_posts_3md." + str(datetime.datetime.now()), reddit_post.target, ex=180)
-                    #
-                    # analise_totals(conn, reddit_post.text)
-                    # analise_most_of(conn, reddit_post.text)
-                    # analise_last_3_minutes(conn)
+                    conn.hmset("reddit_posts." + str(datetime.datetime.now()), reddit_post_data)
+                    conn.set("reddit_posts_3md." + str(datetime.datetime.now()), reddit_post.over_18, ex=180)
+
+                    analise_totals(conn, reddit_post.title)
+                    analise_most_of(conn, reddit_post.title)
+                    analise_last_3_minutes(conn)
 
                 except Exception as ex:
                     print('Error:', ex)
 
-                print("Reddit analysis client received: ", reddit_post.id, reddit_post.title, reddit_post.score, reddit_post.author, reddit_post.original_date, reddit_post.full_link, reddit_post.over_18, flush=True)
+                print("Reddit analysis client received: ",
+                      reddit_post.id, reddit_post.title, reddit_post.score, reddit_post.author, reddit_post.original_date, reddit_post.full_link, reddit_post.over_18, flush=True)
 
 
-def analise_totals(conn, text):
-    word_count = len(re.findall(r'\w+', text))
+def analise_totals(conn, title):
+    word_count = len(re.findall(r'\w+', title))
 
     vowels = 'aeiou'
     vowel_count = 0
 
-    for letter in text.lower():
+    for letter in title.lower():
         if letter in vowels:
             vowel_count += 1
 
@@ -61,23 +62,28 @@ def analise_totals(conn, text):
 
 def analise_last_3_minutes(conn):
     keys = conn.scan_iter("reddit_posts_3md*")
-    sentiments = [0, 0, 0, 0, 0]
+    over_18_count = 0
+    under_18_count = 0
 
     for key in keys:
-        target = int(conn.get(key))
-        sentiments[target] += 1
+        over_18 = str(conn.get(key))
+        if over_18 == "True":
+            over_18_count += 1
+        else:
+            under_18_count += 1
 
-    conn.set("reddit_posts_3_minute_sentiment", str(sentiments))
+    conn.set("reddit_posts_3_minute_over_18", str(over_18_count))
+    conn.set("reddit_posts_3_minute_under_18", str(under_18_count))
 
 
-def analise_most_of(conn, text):
-    word_count = len(re.findall(r'\w+', text))
+def analise_most_of(conn, title):
+    word_count = len(re.findall(r'\w+', title))
 
     ath_word_count = conn.get("reddit_posts_all_time_high_word_count")
 
     if ath_word_count is None or word_count > int(ath_word_count):
         conn.set("reddit_posts_all_time_high_word_count", word_count)
-        conn.set("reddit_posts_all_time_high_word", text)
+        conn.set("reddit_posts_all_time_high_word", title)
 
 
 if __name__ == '__main__':
--- website/website.py
+++ website/website.py
@@ -11,10 +11,12 @@ def print_logs():
     try:
         conn = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
 
-        analytics_html = output_analytics(conn)
-        tweets_html = output_tweets(conn)
+        tweets_analytics_output = tweets_analytics(conn)
+        tweets_table_output = tweets_table(conn)
+        reddit_analytics_output = reddit_posts_analytics(conn)
+        reddit_table_output = reddit_posts_table(conn)
 
-        output += combine_outputs(analytics_html, tweets_html)
+        output += combine_outputs(tweets_analytics_output, tweets_table_output, reddit_analytics_output, reddit_table_output)
 
     except Exception as ex:
         output = 'Error:' + str(ex) + '<META HTTP-EQUIV="refresh" CONTENT="1">'
@@ -22,8 +24,8 @@ def print_logs():
     return output
 
 
-def output_tweets(conn):
-    html = """
+def tweets_table(conn):
+    row_html = """
                  <tr>
                     <td>
                       <div style="background-color:%s;">
@@ -61,32 +63,28 @@ def output_tweets(conn):
         elif int(data["target"]) == 4:
             background_color = "lightblue"
 
-        output += html % (background_color, data["user"], data["text"], data["id"], data["word_count"], data["time_analysed"])
+        output += row_html % (background_color, data["user"], data["text"], data["id"], data["word_count"], data["time_analysed"])
 
     return output
 
 
-def output_analytics(conn):
+def tweets_analytics(conn):
     html = """
-              <div id="first">
-            
-                <div style="text-align: center;"><h3> All Time Total </h3></div>
-                Total vowels: %s
-                <br>
-                Total Words: %s 
-                <br>
-                Average vowels per word: %s
-            
-            
-                <div style="text-align: center;"><h3> 3 Minute Sentiment </h3></div>
-                %s
-            
-                <div style="text-align: center;"><h3> Most of... </h3></div>
-                <strong>Tweet with most words:</strong> %s 
-                <br><br>
-                Word count: %s
-            
-              </div>
+            <div style="text-align: center;"><h3> All Time Total </h3></div>
+            Total vowels: %s
+            <br>
+            Total Words: %s 
+            <br>
+            Average vowels per word: %s
+        
+        
+            <div style="text-align: center;"><h3> 3 Minute Sentiment </h3></div>
+            %s
+        
+            <div style="text-align: center;"><h3> Most of... </h3></div>
+            <strong>Tweet with most words:</strong> %s 
+            <br><br>
+            Word count: %s
            """
 
     total_vowel_count = str(conn.get("total_vowel_count"))
@@ -108,7 +106,82 @@ def output_analytics(conn):
     return html % (total_vowel_count, total_word_count, round(int(total_vowel_count) / int(total_word_count), 2), sentiment_message, ath_word, ath_word_count)
 
 
-def combine_outputs(analytics_html, tweets_html):
+def reddit_posts_table(conn):
+    html = """
+                <tr>
+                    <td>
+                        <div style="text-align: center;">
+                            <small><i>%s</i></small>
+                
+                            <br>
+                
+                            %s
+                
+                            <br>
+                
+                            <small>
+                                Reddit URL: %s
+                            </small>
+                
+                            <br>
+                
+                            <small>
+                                <strong> Reddit Post Id: </strong> %s
+                                <strong> Words:</strong> %s
+                                <strong> Time analysed: </strong> %s
+                            </small>
+                        </div>
+                    </td>
+                </tr>
+           """
+
+    keys = conn.scan_iter("reddit_posts.*")
+    keys = sorted(keys, reverse=True)
+
+    output = ''
+
+    for key in keys:
+        data = conn.hgetall(key)
+
+        output += html % (data["author"], data["title"], data["full_link"], data["id"], data["word_count"], data["time_analysed"])
+
+    return output
+
+
+def reddit_posts_analytics(conn):
+    html = """
+            <div style="text-align: center;"><h3> All Time Total </h3></div>
+            Total vowels: %s
+            <br>
+            Total Words: %s 
+            <br>
+            Average vowels per word: %s
+        
+        
+            <div style="text-align: center;"><h3> 3 Minute Sentiment </h3></div>
+            Over 18: %s
+            <br>
+            Under 18: %s
+        
+            <div style="text-align: center;"><h3> Most of... </h3></div>
+            <strong>Reddit Post with most words:</strong> %s 
+            <br><br>
+            Word count: %s
+           """
+
+    total_vowel_count = str(conn.get("reddit_posts_total_vowel_count"))
+    total_word_count = str(conn.get("reddit_posts_total_word_count"))
+
+    over_18_count = conn.get("reddit_posts_3_minute_over_18")
+    under_18_count = conn.get("reddit_posts_3_minute_under_18")
+
+    ath_word = str(conn.get("reddit_posts_all_time_high_word"))
+    ath_word_count = str(conn.get("reddit_posts_all_time_high_word_count"))
+
+    return html % (total_vowel_count, total_word_count, round(int(total_vowel_count) / int(total_word_count), 2), over_18_count, under_18_count, ath_word, ath_word_count)
+
+
+def combine_outputs(tweets_analytics, tweets_table, reddit_analytics, reddit_table):
     output = """
             <META HTTP-EQUIV="refresh" CONTENT="1.5">
             <style> 
@@ -116,11 +189,11 @@ def combine_outputs(analytics_html, tweets_html):
                 display: flex;
               }
               #first {
-                width: 30%;
+                width: 50%;
                 padding: 1em;
               }
               #second {
-                width: 70%;
+                width: 50%;
                 padding: 1em;
               }
               h3 {
@@ -129,13 +202,31 @@ def combine_outputs(analytics_html, tweets_html):
             </style>
             <div id="wrapper">
             """
-    output += analytics_html
+    # Left side
+    output += """
+              <div id="first">
+              """
+    output += tweets_analytics
+    output += """
+                  
+                <table style="width:100%">
+              """
+    output += tweets_table
+    output += """
+                </table>
+              </div>
+    """
+
+    # Right side
     output += """
               <div id="second">
-            
+              """
+    output += reddit_analytics
+    output += """              
                 <table style="width:100%">
-                """
-    output += tweets_html
+              """
+    output += reddit_table
+
     output += """
                 </table>
               </div>
